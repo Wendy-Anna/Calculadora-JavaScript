df726c1e6beec72522ccb44744e665854d3522b9
Wendy-Anna <wendyufpb@gmail.com>
2020-04-08 10:42:15 -0300

    Manipulando o DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,23 +2,36 @@ class CalcController{
 
     constructor(){
         this._displayCalc = "0";
-        this._dataAtual;
+        this._currentDate;
+        this.initialize();
     }
 
     get displayCalc(){
         return this._displayCalc;
     }
 
+    initialize(){
+
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "0";
+        dateEl.innerHTML = "08/04/2020";
+        timeEl.innerHTML = "10:39";
+
+    }
+
     set displayCalc(valor){
         this._displayCalc = valor;
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    get currentDate(){
+        return this._currentDate;
     }
 
-    set dataAtual(valor){
-        this._dataAtual = valor;
+    set currentDate(valor){
+        this._currentDate = valor;
     }
 
     
